949e5e1f31d78d5c7ccb8d15ab3d86ab0794fab2
Houcem Hedhly <hedhoucem@gmail.com>
2021-10-25 09:11:59 +0100

    add and center image

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
-        title: Text('I am rich'),
+        title: Center(child: Text('I am rich')),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg'),
+        ),
       ),
     ),
   ));
